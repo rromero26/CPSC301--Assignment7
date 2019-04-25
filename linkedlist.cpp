51f94f21a8d6c3f41ee948f4815127f160c026c6
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2019-04-25 11:05:54 -0700

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,31 +1,40 @@
 #include "linkedlist.h"
 
 linkedlist::linkedlist() {
-
+    this->head = nullptr;
+    this-> count() = 0;
+    this-> sum() = 0;
 }
 
-linkedlist::~linkedlist() {
-    // RECURSIVE
+linkedlist::~linkedlist(struct node *head) {
+    if(head == NULL)
+        {return;}
+    ~linkedlist (head->next);
+    delete head;
 }
 
-int linkedlist::count() {
-    // RECURSIVE
+int linkedlist::count(struct node* head) {
+    if (head == NULL)
+        {return 0;}
+    return 1 + count(head->next);
 }
 
 void linkedlist::addToFront(int n) {
-
+    //
 }
 
-double linkedlist::average() {
+double linkedlist::average(struct *node head) {
     // uses sum and count functions
 }
 
-int linkedlist::sum() {
-    // RECURSIVE
+int linkedlist::sum(struct node *head) {
+    if(head == NULL)
+        {return 0;}
+    return head->data + sum(head->next);
 }
 
 void linkedlist::writeInorder(string & file) {
-
+    //
 }
 
 void linkedlist::writeReversed(string & file) {
