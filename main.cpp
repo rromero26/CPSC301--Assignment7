1967b139e18047583c938c8a30f8b30e81cd7f5c
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2019-04-26 20:47:04 -0700

    checkpoint

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <string>
 #include "linkedlist.cpp"
-
+#include <fstream>
 using namespace std;
 
-void readFile(linkedlist & list, string & file) {
+void readFile(linkedlist & myList, string & file) {
+    fstream myFile;
+    myFile.open(file);
 
+    if(!myFile){                                      //check if file open correctly
+      cout << "Error, could not open file." << endl;
+      exit(1);
+    }
+
+    int tempVal;
+    while(myFile >> tempVal){
+      addSort(tempVal);                     //adds onto linklist in order
+      //linkedlist function in order
+    }
+    myFile.close();
 }
 
+
+// ------------------------- MAIN -----------------------------
+
 int main() {
+    linkedlist();                  //SHOULD create linkedlist
+    string userFile;
+    cout << "Enter file name." << endl;
+    cin >> userFile;                            //SHOULD get string of file.txt
+
+    readFile(userFile);             //SHOULD store and sort linkedlist in order
+
+    cout << "The average of LinkedList is: " << average() << endl;
+
+    writeInorder(userFile);         //SHOULD write into file in order (since stored in order)
+
+    cout << "SUCCESS: data written, in order, back into file" << endl;
+
+    writeReversed(userFile);        //SHOULD write into file in reverse order
+
+    cout << "SUCCESS: data written, in reverse order, back into file"
+
+    ~linkedlist();
 
     return 0;
 }
